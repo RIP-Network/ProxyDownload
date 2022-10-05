e4c43cdcdfc254f3474a61806d71a9a286124bf7
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-10-05 19:27:06 +0200

    Update Proxy.sh

--- Proxy.sh
+++ Proxy.sh
@@ -4,16 +4,9 @@ echo "Creado por RIP-Network V 1.0"
 sleep 3
 clear
 
-echo " ██▓███   ██▀███   ▒█████  ▒██   ██▒▓██   ██▓▓█████▄  ▒█████   █     █░███▄    █  ██▓     ▒█████   ▄▄▄      ▓█████▄ "
-echo " ▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒▒▒ █ █ ▒░ ▒██  ██▒▒██▀ ██▌▒██▒  ██▒▓█░ █ ░█░██ ▀█   █ ▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌"
-echo " ▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒░░  █   ░  ▒██ ██░░██   █▌▒██░  ██▒▒█░ █ ░█▓██  ▀█ ██▒▒██░    ▒██░  ██▒▒██  ▀█▄  ░██   █▌"
-echo " ▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░ ░ █ █ ▒   ░ ▐██▓░░▓█▄   ▌▒██   ██░░█░ █ ░█▓██▒  ▐▌██▒▒██░    ▒██   ██░░██▄▄▄▄██ ░▓█▄   ▌"
-echo " ▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░▒██▒ ▒██▒  ░ ██▒▓░░▒████▓ ░ ████▓▒░░░██▒██▓▒██░   ▓██░░██████▒░ ████▓▒░ ▓█   ▓██▒░▒████▓ "
-echo " ▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒▒ ░ ░▓ ░   ██▒▒▒  ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▓░▒ ▒ ░ ▒░   ▒ ▒ ░ ▒░▓  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ▒▒▓  ▒ "
-echo " ░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░ ░░   ░▒ ░ ▓██ ░▒░  ░ ▒  ▒   ░ ▒ ▒░   ▒ ░ ░ ░ ░░   ░ ▒░░ ░ ▒  ░  ░ ▒ ▒░   ▒   ▒▒ ░ ░ ▒  ▒ "
-echo " ░░         ░░   ░ ░ ░ ░ ▒   ░    ░   ▒ ▒ ░░   ░ ░  ░ ░ ░ ░ ▒    ░   ░    ░   ░ ░   ░ ░   ░ ░ ░ ▒    ░   ▒    ░ ░  ░ "
-echo "             ░         ░ ░   ░    ░   ░ ░        ░        ░ ░      ░            ░     ░  ░    ░ ░        ░  ░   ░    "
-echo "                                      ░ ░      ░                                                              ░      "
+neofetch
+
+sleep 3
 
 while :
 do
@@ -34,20 +27,26 @@ read opcion
 case $opcion in
 1) echo "SOCKS5"
 echo "Descargando..."
+sleep 2
 curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt -o socks5.txt
 echo "SOCKS5.txt se encuentra en esta carpeta"
+sleep 5
 
 ;;
 2) echo "SOCKS4"
 echo "Descargando..."
+sleep 2
 curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o socks4.txt
 echo "SOCKS4.txt se encuentra en esta carpeta"
+sleep 5
 
 ;;
 3) echo "HTTP-HTTPS"
 echo "Descargando..."
+sleep 2
 curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o http.txt
 echo "HTTP-HTTPS.txt se encuentra en esta carpeta"
+sleep 5
 
 ;;
 4)exit 0;;
